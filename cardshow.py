95975ec469519bddd4ff128a15e936f95a3fb0c8
petru <petrumadar@gmail.com>
2020-02-17 00:07:09 +0200

    Add encoding

--- cardshow.py
+++ cardshow.py
@@ -2,6 +2,7 @@ import os
 page_header = """
 <html>
     <head>
+        <meta charset="utf-8">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css" integrity="sha256-7LuOHbsBImoaCHWzjqQDLeGK9kq/rZZqIr6Gtkz0WzI=" crossorigin="anonymous" />
         <link href="https://fonts.googleapis.com/css?family=Farro|Oswald|Roboto+Condensed&display=swap" rel="stylesheet"> 
         <style type="text/css">
